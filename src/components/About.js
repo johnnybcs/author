7fc50e882dcf50b6736599ad746a523776ae22e0
johnnybcs <johnny@ualberta.ca>
2020-05-26 02:10:47 -0700

    improved gui

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Typical from 'react-typical';
 import Navigation from './Navigation';
 import Footer from './Footer';
-
 import './style.css';
 
 const About = () => {
@@ -12,26 +11,42 @@ const About = () => {
             <div className="app background-about">
                 <br />
                 <br />
-                <div className="ui card">
-                    <div className="image">
-                        <img src="headShot.jpg" alt='author' />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <div class="ui move up reveal">
+                    <div class="visible content">
+                        <img class="ui image" src="curtains.jpg" alt="curtains" />
                     </div>
-                    <div className="content">
-                        <div className="header-text">
-                            Hi, I'm Johnny
-                        </div>
-                        <br />
-                        <div className="description">
-                            I'm{' '}
-                            <Typical
-                                steps={[
-                                    'an author ✍️', 1500,
-                                    'a developer ☕', 1500,
-                                    'a student 📚', 1500
-                                ]} 
-                                loop={Infinity}
-                                wrapper="b"
-                            />
+                    <div class="hidden content">
+                        <div className="ui card">
+                            <div className="image">
+                                <img src="headShot.jpg" alt='author' />
+                            </div>
+                        <div className="content">
+                            <div className="header-text">
+                                Hi, I'm Johnny
+                            </div>
+                            <br />
+                            <div className="description">
+                                I'm{' '}
+                                <Typical
+                                    steps={[
+                                        'an author ✍️', 1500,
+                                        'a developer ☕', 1500,
+                                        'a student 📚', 1500
+                                    ]} 
+                                    loop={Infinity}
+                                    wrapper="b"
+                                />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
