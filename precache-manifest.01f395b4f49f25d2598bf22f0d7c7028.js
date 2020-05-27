4991a13e4dea43b2a3b4531a9e95442437a00fe3
johnnybcs <johnny@ualberta.ca>
2020-05-26 23:48:58 -0700

    Updates

---
+++ precache-manifest.01f395b4f49f25d2598bf22f0d7c7028.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0805af99efd27a2d8a91e254cf09f0",
+    "url": "/author/index.html"
+  },
+  {
+    "revision": "209cb45d8de39c3dbe5d",
+    "url": "/author/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "99c44180d3c94fea60cd",
+    "url": "/author/static/css/main.ad185fce.chunk.css"
+  },
+  {
+    "revision": "209cb45d8de39c3dbe5d",
+    "url": "/author/static/js/2.2ef3a8d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/author/static/js/2.2ef3a8d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c44180d3c94fea60cd",
+    "url": "/author/static/js/main.b3399883.chunk.js"
+  },
+  {
+    "revision": "a65da9473c385295c1fe",
+    "url": "/author/static/js/runtime-main.bec75ccf.js"
+  },
+  {
+    "revision": "ec5e7062f23911a25c4412045786c6cd",
+    "url": "/author/static/media/background-about.ec5e7062.jpg"
+  },
+  {
+    "revision": "96a2bd8fe44e51810e78c75a1bdda116",
+    "url": "/author/static/media/background-contact.96a2bd8f.jpg"
+  }
+]);
